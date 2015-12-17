db34957dfbab058455863787f1506d6c95788f11
Xiaming Chen <chenxm35@gmail.com>
2015-12-17 22:41:39 +0800

    Finish powerlaw comparison

--- src/030.mobility3d.R
+++ src/030.mobility3d.R
@@ -6,8 +6,14 @@ library(magicaxis)
 library(movr)
 library(rgl)
 
-mov <- readRDS("rdata/hcl.rds") %>% filter(uid <= 1000)
-bm <- readRDS("rdata/hcl_bm.rds")
+filter <- dplyr::filter
+
+mov <- fread("data/hcl_mesos7d", sep=",", head=F)
+setnames(mov, c("uid", "time", "bid"))
+mov <- mov %>% as.data.frame %>% filter(uid <= 1000)
+
+bm <- fread("data/hcl_mesos7d_bm", sep=",", head=F, colClasses=c("integer", "character", "numeric", "numeric"))
+setnames(bm, c("bid", "bs", "lon", "lat"))
 bm <- bm %>% filter(lon >= 120.0 & lon <= 120.5)
 
 # calculate relative coordinates in KM
@@ -25,120 +31,13 @@ bm <- ddply(bm, c("bid"), function(r) {
 })
 
 # merge bm into mov data
-mov <- left_join(mov, bm, by=c("bid"="bid"))
+mov2 <- mov %>% as.data.frame %>% left_join(bm, by=c("bid"="bid"))
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# VISUALIZATION OF MOBILITY
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+users <- subset(movement, id %in% c(23, 20)) %>%
+  mutate(time = time/86400 - min(time/86400)) %>%
+  dplyr::filter(time <= 30)
 
-# plot 3D voronoi canvas
-voronoi3d <- function(points, side, col.seg = "grey", col.point=col.seg, lty=1, lwd=1) {
-  stopifnot(tolower(side) %in% c('x', 'y', 'z'))
-  if (is.data.frame(points) || is.matrix(points))
-    points = list(points)
-  col.point = as.vector(col.point)
-  if (length(col.point) == 1)
-    col.point = rep(col.point, length(points))
-  else
-    stopifnot(length(col.point) == length(points))
-  bbox = par3d('bbox')
-  br = range(bbox)
-  if (br[1] - br[2] > 3e+30)
-    stop("A valid rgl canvas is needed first when calling voronoi3d{movr}.")
-  xr = bbox[1:2]
-  yr = bbox[3:4]
-  zr = bbox[5:6]
-  side = tolower(side)
-  coord = cbind(xr, yr, zr)[,which(side == c('x', 'y', 'z'))]
-  
-  plot.direchlet.tess <- function() {
-    points.all = do.call("rbind", points) %>% distinct_
-    dd = deldir(points.all[,1], points.all[,2])
-    dirsgs = dd$dirsgs
-    p1 = as.vector(t(dirsgs[,c(1,3)]))
-    p2 = as.vector(t(dirsgs[,c(2,4)]))
-    p3 = rep(coord[1], 2)
-    if (side == 'x') {
-      x = p3
-      y = p1
-      z = p2
-    } else if (side == 'y') {
-      x = p1
-      y = p3
-      z = p2
-    } else {
-      x = p1
-      y = p2
-      z = p3
-    }
-    rgl.lines(x=x, y=y, z=z, lwd=lwd, lty=lty, color=col.seg)
-  }
-  plot.direchlet.tess()
-  
-  plot.points <- function(points, col) {
-    points = as.data.frame(points) %>% distinct_
-    s1 = as.vector(points[,1])
-    s2 = as.vector(points[,2])
-    s3 = rep(coord[1], 2)
-    if (side == 'x') {
-      px = s3
-      py = s1
-      pz = s2
-    } else if (side == 'y') {
-      px = s1
-      py = s3
-      pz = s2
-    } else {
-      px = s1
-      py = s2
-      pz = s3
-    }
-    rgl.points(x=px, y=py, z=pz, color=col, alpha=0.5)
-  }
-  
-  for ( i in 1:length(points)) {
-    plot.points(points[i], col.point[i])
-  }
-}
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# select one user
-user <- mov %>% filter(uid == 1 & time - min(time) <= 86600 * 30)
-user2 <- mov %>% filter(uid == 12 & time - min(time) <= 86600 * 30)
-user3 <- mov %>% filter(uid == 1 & time - min(time) <= 86600 * 30)
-#++++++++++++++++++++++++++++++++++++++
-par3d(windowRect=c(20,40,800,800))
-rgl.clear()
-rgl.clear("lights")
-rgl.bg(color="white")
-rgl.viewpoint(theta = 65, phi = 15)
-rgl.light(theta = 45, phi = 45, viewpoint.rel=TRUE)
-
-mobility3d <- function(x, y, t, size=2.5, col=colors()[sample(1:255,1)],
-                         xlab="", ylab="", zlab="", add=FALSE, ...) {
-  plot3d(x, t, y, type='p', size=2.5, col=col, xlab=xlab, ylab=ylab, zlab=zlab, add=add, ...)
-  lines3d(x, t, y, color=col, ...)
-}
-
-col1 = colors()[136]
-col2 = colors()[122]
-col3 = colors()[51]
-mobility3d(user$x, user$y, user$t-min(user$t), col=col1)
-mobility3d(user2$x, user2$y, user2$t-min(user2$t), col=col2, add=T)
-mobility3d(user3$x, user3$y, user3$t-min(user3$t), col=col3, add=T)
-
-points = list(user[, c('x', 'y')] %>% distinct,
-              user2[, c('x', 'y')] %>% distinct,
-              user3[, c('x', 'y')] %>% distinct)
-voronoi3d(points, side='y')
-
-title3d(xlab="East-West (km)", ylab="Time", zlab="North-South (km)")
-axes3d(lwd=0.7, xlen=8, ylen=10, zlen=8, col='black', marklen=40)
-grid3d(side=c("x", "z"), lwd=0.7, lty=2)
-
-for(i in seq(65, 360+65, by = 0.15)) {
-  rgl.viewpoint(theta = -90+i, phi =30)
-}
-
+movr::draw_mobility3d(users$lon, users$lat, users$time,
+           group_by=users$id, col=c('royalblue', 'orangered'))
 rgl.postscript("figures/mobility3d.pdf", fmt="pdf")
 rgl.close()
--- src/101.powerlaw.R
+++ src/101.powerlaw.R
@@ -1,60 +1,88 @@
 # Estimate the distributions of mobility graph links
-library(dplyr)
+library(movr)
 library(igraph)
 library(magicaxis)
 library(sfsmisc)
-library(movr)
 
-fm.d <- read.csv("data/scl_fine_p06_fm_i24h", sep=',', header=F, stringsAsFactors=F)
+## Senegal
+fm.d <- read.csv("data/scl_fine_p12_fm_i24h", sep=',', header=F, stringsAsFactors=F)
 colnames(fm.d) = c("interval", "from", "to", "unique", "total")
 fm.d$interval = as.POSIXct(fm.d$interval * 3600 * 24, origin="1970-01-01 00:00:00", tz="GMT")
+graph.d <- as.undirected(graph.data.frame(fm.d[, c("from", "to")]))
+degree.d <- as.numeric(degree(graph.d))
+hist.d <- hist(degree.d, breaks=c(0:max(degree.d)), plot=F)
 
+## Hangzhou
 fm.h <- read.csv("data/hcl_mesos0825_fm_i6h", sep=',', header=F, stringsAsFactors=F)
 colnames(fm.h) = c("interval", "from", "to", "unique", "total")
 fm.h$interval = as.POSIXct(fm.h$interval * 3600 * 6, origin="1970-01-01 00:00:00", tz="Asia/Shanghai")
-fm.h <- fm.h[fm.h$interval==as.POSIXct("2012-08-25 14:00:00", tz="Asia/Shanghai"), ]
-
-trips <- read.csv("data/tts_trips.dat", head=T, sep=",")
-trips <- trips[complete.cases(trips), ]
-fm.t <- trips %>% dplyr::filter(DAYNO == 1) %>%
-  with(flowmap2(uid, locno, as.numeric(stime), as.numeric(etime))) %>%
-  dplyr::filter(from != to) %>%
-  mutate(from = as.numeric(from), to = as.numeric(to))
-
-graph.d <- as.undirected(graph.data.frame(fm.d[, c("from", "to")]))
+# fm.h <- fm.h[fm.h$interval==as.POSIXct("2012-08-25 14:00:00", tz="Asia/Shanghai"), ]
 graph.h <- as.undirected(graph.data.frame(fm.h[, c("from", "to")]))
-graph.t <- as.undirected(graph.data.frame(fm.t[, c("from", "to")]))
-
-degree.d <- as.numeric(degree(graph.d))
-hist.d <- hist(degree.d, breaks=c(0:max(degree.d)), plot=F)
 degree.h <- as.numeric(degree(graph.h))
 hist.h <- hist(degree.h, breaks=c(0:max(degree.h)), plot=F)
-degree.t <- as.numeric(degree(graph.t))
-hist.t <- hist(degree.t, breaks=c(0:max(degree.t)), plot=F)
 
-pdf("figures/powerlaw.pdf", width=6.5, height=5)
+## Chicago
+# trips <- read.csv("data/tts_trips.dat", head=T, sep=",")
+# trips <- trips[complete.cases(trips), ]
+# fm.t <- trips %>% dplyr::filter(DAYNO == 1) %>%
+#   with(flowmap2(uid, locno, as.numeric(stime), as.numeric(etime))) %>%
+#   dplyr::filter(from != to) %>%
+#   mutate(from = as.numeric(from), to = as.numeric(to))
+# graph.t <- as.undirected(graph.data.frame(fm.t[, c("from", "to")]))
+# degree.t <- as.numeric(degree(graph.t))
+# hist.t <- hist(degree.t, breaks=c(0:max(degree.t)), plot=F)
+
+## SJTU
+# fm.j <- read.csv('data/wifi_build_151214_fm_i24h', head=F, sep=',')
+# colnames(fm.j) <- c('interval', 'from', 'from_lat', 'from_lon', 'to', 'to_lat', 'to_lon', 'unique', 'total')
+# fm.j$interval = as.POSIXct(fm.j$interval * 3600 * 24, origin="1970-01-01 00:00:00", tz="Asia/Shanghai")
+
+fm.j <- read.csv('data/wifi_ap_140914_fm_i24h', head=F, sep=',')
+colnames(fm.j) <- c('interval', 'from', 'to', 'unique', 'total')
+fm.j$interval = as.POSIXct(fm.j$interval * 3600 * 24, origin="1970-01-01 00:00:00", tz="Asia/Shanghai")
+
+graph.j <- as.undirected(graph.data.frame(fm.j[, c("from", "to")]))
+degree.j <- as.numeric(degree(graph.j))
+hist.j <- hist(degree.j, breaks=c(0:max(degree.j)), plot=F)
+
+############################
+## Plot powerlaws
+############################
+pdf("figures/allthree_powerlaw.pdf", width=5, height=4)
 par(mar=c(3.5, 3.5, 1, 1))
+xlim=c(1, 200); ylim=c(10^-5, 0.7)
+magplot(seq(1, 200, length.out = 10), log='xy', seq(ylim[1], ylim[2], length.out=10), type = "n",
+        xlim=xlim, ylim=ylim, xlab="Degree of Vertex", ylab="PDF", side=1:4, labels=c(T,T,F,F))
+
+cols = c('royalblue', 'orangered', 'slategray', 'sandybrown')
 
 ## Senegal
 x = ceiling(hist.d$mids)
 y = hist.d$density
-magplot(x, y, log="xy", pch=21, bg='royalblue', cex=1.2, col='royalblue',
-        xlim=c(1, 200), ylim=c(10^-5, 0.7), xlab="Degree of Vertex", ylab="PDF", side=1:4, labels=c(T,T,F,F))
-fit_truncated_power_law(x, y, x0=80)
+points(x, y, log="xy", pch=0, cex=1, col=cols[1])
+fit_truncated_power_law(x, y, xmax=80, col=cols[1], lty=1)
 
 ## Hangzhou
 x = ceiling(hist.h$mids)
 y = hist.h$density
-points(x, y, log="xy", pch=21, cex=1.2, col='orangered', bg='orangered')
-fit_truncated_power_law(x, y, x0=25)
+points(x, y, log="xy", pch=6, cex=1, col=cols[2])
+fit_truncated_power_law(x, y, xmax=25, col=cols[2], lty=2)
 
 ## Chicago
-x = ceiling(hist.t$mids)
-select=c(3:length(x))
-x = x[select]
-y = hist.t$density[select]
-points(x, y, log='xy', pch=21, bg=colors()[558], col="white", cex=1.2)
-fit_power_law(x, y, x0 = 13)
+# x = ceiling(hist.t$mids)
+# select=c(3:length(x))
+# x = x[select]
+# y = hist.t$density[select]
+# points(x, y, log='xy', pch=21, cex=1, col=cols[3])
+# fit_power_law(x, y, x0 = 13)
+
+## SJTU
+x = ceiling(hist.j$mids)
+y = hist.j$density
+points(x, y, log="xy", pch=20, cex=1, col=cols[4])
+fit_truncated_power_law(x, y, xmax=70, col=cols[4], lty=5)
+
+legend(1.5, 0.0005, legend=c("Country", "City", "Campus"), pch=c(0,6,20), col=cols)
 
 dev.off()
 
